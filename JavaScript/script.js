4914698b182d120546d98fc4390bc67bc014dbc2
Willian Sousa <williansousa4k@gmail.com>
2023-12-24 16:29:55 -0300

    Atividade 2 concluida

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -13,13 +13,13 @@ ul.innerHTML = "<li>Carro</li>" + "<li>Moto</li>" + "<li>Pedestre</li>"
 
 let minhaLista = document.querySelector("#lista-ordenada");
 
-        var itens = [
+        let itens = [
             { texto: "Linux-confih", link: "https://linuxconfig.org/linux-commands-cheat-sheet" },
             { texto: "Google-fonts", link: "https://fonts.google.com/" },
             { texto: "stackoverflow", link: "https://stackoverflow.com/" }
         ];
 
-        var listaHTML = "";
+        let listaHTML = "";
         for (var i = 0; i < itens.length; i++) {
             listaHTML += "<li><a href='" + itens[i].link + "'>" + itens[i].texto + "</a></li>";
         }
